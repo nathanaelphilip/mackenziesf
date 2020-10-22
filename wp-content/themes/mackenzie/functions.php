a38a2a8dd3fac9e21d29fb92c3dd5cf408b6f092
Nathanael Moody <hello@nathanaelphilip.com>
2020-10-22 15:32:59 -0500

    adds filemtime to style

--- wp-content/themes/mackenzie/functions.php
+++ wp-content/themes/mackenzie/functions.php
@@ -4,7 +4,7 @@
 
   function my_theme_enqueue_styles() {
     $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css', filemtime(get_template_directory() . '/style.css') );
   }
 
   set_theme_mod('beckett_recent_projects_count', 12);
